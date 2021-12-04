f5cc326dd466523008633fdb067be8fd5dd01926
Kienntpk01531 <kienntpk01531@fpt.edu.vn>
2021-12-04 16:59:44 +0700

    comment here

---
+++ lab5.js
@@ -0,0 +1,6 @@
+// const todo = document.getElementById("txtUser").value;
+//   const listTodo = document.getElementById("lstTodo");
+//   const liElm = document.createElement("li");
+//   const textNode = document.createTextNode(todo);
+//   liElm.appendChild(textNode);
+//   listTodo.appendChild(liElm);
